29fb1c7bac42f307daa9253af6e46c0d322e89f2
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2024-02-25 15:51:04 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,13 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+ apiKey: "AIzaSyBe3324xn6D2mG25WXbshJeHynbS1Q3aBY",
+
+  authDomain: "baro-6p3kmc.firebaseapp.com",
+
+  projectId: "baro-6p3kmc",
+
+  storageBucket: "baro-6p3kmc.appspot.com",
+
+  messagingSenderId: "301982493416",
+
+  appId: "1:301982493416:web:ca7f770d6a9e3e119a375d"
 };
